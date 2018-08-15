a0317ea7ddb99b7a883dd22f3b6e35d3b93cab3e
akbarsyamisid <akbar.syam@isid.co.jp>
2018-08-15 15:22:40 +0900

    Remove ununsed files
    setup[1-2].bash are not required anymore

--- setup1.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/raspberrypi/RaspberryPiMouse/lib/Pi2B+/4.14.50-v7+/   
-/sbin/insmod rtmouse.ko
--- setup2.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xve
-
-exec 2> /tmp/setup.log
-
-cd /home/raspberrypi/RaspberryPiMouse/lib/Pi2B+/4.14.50-v7+/   
-/sbin/insmod rtmouse.ko
